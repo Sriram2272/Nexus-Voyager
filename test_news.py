90b3dda81b946faa6feed7f8df4ddc2a1885c175
Dama Sri Ram <sriramdama417@gmail.com>
2025-10-22 12:34:12 +0530

    Delete test_news.py

--- test_news.py
+++
@@ -1,5 +0,0 @@
-import requests, json
-
-r = requests.post("http://127.0.0.1:8000/run", json={"query":"Give me top 3 technology news"})
-print("Status:", r.status_code)
-print("Response:", r.json())
